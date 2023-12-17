779cf42c71cac3b6cc49ba107a91b71dd07c0424
MianMuhammadBodla <mianmuhammadbodla@gmail.com>
2023-12-17 23:53:47 +0500

    updadegzdghxdfd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,10 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-   <div>WELCOME WORLD</div>
-   
+    <main className=" px-4 py-4 text-green-900">
+   <div className='font-bold text-yellow-400'>WELCOME WORLD</div>
+   <p className='text-red-300'>THIS is zoom online created project</p>
+   <p>rsregf</p>
     </main>
   )
 }
