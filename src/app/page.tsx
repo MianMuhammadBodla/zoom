36a42442bb43c5ec2395cce33357c29cf7e38bd7
MianMuhammadBodla <mianmuhammadbodla@gmail.com>
2023-12-21 22:38:41 +0500

    updsdsgdgd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
 import Image from 'next/image'
-
+import piaic from "../../public/ai.jpg"
 export default function Home() {
-  return (
-    <main className=" px-4 py-4 text-green-900">
+  return ( 
+    <main className=" px-4 py-4 text-green-900 ">
    <div className='font-bold text-yellow-400'>WELCOME WORLD</div>
+  <Image 
+  src={piaic}
+  width={300}
+  height={250}
+  alt={''}
+  className=' border-4 rounded-md border-blue-500'
+  ></Image>
+
    <p className='text-red-300'>THIS is zoom online created project</p>
    <p>rsregf</p>
     </main>
